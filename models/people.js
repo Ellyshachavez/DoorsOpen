4019c217ae50f91838a501d4ea9c7d65686dab31
Leah Weston <westonl91@yahoo.com>
2019-05-09 19:02:08 -0700

    initial push

---
+++ models/people.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const peopleSchema = new Schema({
+  agent: { type: Boolean, required: true },
+  loanOfficer: { type: Boolean, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  company: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  profilePic: String,
+  events: {
+    address: String,
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zip: { type: Number, required: true },
+    date: { type: String, required: true },
+    time: { type: String, required: true },
+    registrations: {
+      name: { type: String, required: true },
+      phone: { type: String, required: true },
+      email: { type: String, required: true }
+    }
+  }
+});
+
+const People = mongoose.model("People", peopleSchema);
+
+module.exports = People;
