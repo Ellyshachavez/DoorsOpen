2589328d657cfe10f991f35ef0c0f6b86166650c
Ellysha Chavez <ellyshachavez@gmail.com>
2019-06-06 16:53:26 -0700

    deploy

--- client/src/components/Avatar/Avatar.js
+++ client/src/components/Avatar/Avatar.js
@@ -1,13 +1,37 @@
+import { Avatar } from "antd";
+
+class Avatar extends Component {
+    static contextType = UserContext;
+  
+    state = {
+      id: "",
+      profilePic: ""
+    };
+  
+  
+    render() {
+      return (
+      <UserContext.Consumer>
+         {({user}) => (
+        <div>
+            <Image cloudName="demo" publicId="sample" width="300" crop="scale"/>
+            // Or for more advanced usage:
+            // import {CloudinaryContext, Transformation} from 'cloudinary-react';
+            <CloudinaryContext cloudName="demo">
+                <Image publicId="sample">
+                    <Transformation width="200" crop="scale" angle="10"/>
+                </Image>
+            </CloudinaryContext>
+        </div>
+         )}
+          </UserContext.Consumer>
+      );
+    }
+  }
+  
+  export default Avatar;
+
 ReactDOM.render(
-    <div>
-        <Image cloudName="demo" publicId="sample" width="300" crop="scale"/>
-        // Or for more advanced usage:
-        // import {CloudinaryContext, Transformation} from 'cloudinary-react';
-        <CloudinaryContext cloudName="demo">
-            <Image publicId="sample">
-                <Transformation width="200" crop="scale" angle="10"/>
-            </Image>
-        </CloudinaryContext>
-    </div>,
+  ,
     document.getElementById('Avatar')
 );
